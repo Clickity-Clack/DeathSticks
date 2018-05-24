c88a9236117be5a7313ba39150361501153277a8
Clickity_Clack <spaceman.bob@live.com>
2018-05-24 10:30:34 -0400

    updated collision

--- weapons/projectiles/Bullet.lua
+++ weapons/projectiles/Bullet.lua
@@ -4,21 +4,13 @@ local Bullet = class('Bullet', Projectile)
 function Bullet:initialize(weapon, world)
     assert(self.damage)
     Projectile.initialize(self, weapon, world)
+    initCollisions(self.collisions)
 end
 
-function Bullet:doop(aCharacter)
-    if self.playerId ~= aCharacter.playerId then
-        aCharacter.Health = aCharacter.Health - self.damage
-        if aCharacter.Health <= 0 then
-            aCharacter.dead = true
-            aCharacter.lastBullet = self
-        end
+function initColisions(collisions)
+    collisions.Platform = function(self, Platform)
         self.dead = true
     end
-end
-
-function Bullet:collidePlatform(Platform)
-    self.dead = true
 end
 
 return Bullet
