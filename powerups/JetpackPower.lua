65ee02648bc2f4fcfcd0f84f249fdd9de13986f9
Clickity-Clack <spaceman.bob@live.com>
2018-08-14 18:09:50 -0400

    aesthetic change

--- powerups/JetpackPower.lua
+++ powerups/JetpackPower.lua
@@ -3,9 +3,10 @@ local Jetpack = require 'character/Jetpack'
 local JetpackPower = class('JetpackPower', Powerup)
 
 function JetpackPower:initialize( body )
-    local image = love.graphics.newImage('res/rocket.png')
+    local image = love.graphics.newImage('res/jetpack.png')
     Powerup.initialize(self, body, image)
     self.value = 50
+    self.scale = 1
 end
 
 function JetpackPower:zoop(aCharacter)
