bc7aba46ddb7d31686a19ff2c0ceb1347a709da3
Clickity-Clack <spaceman.bob@live.com>
2019-06-14 15:12:56 -0500

    Started working on tests for inflater

--- spec/unpack/Inflater-spec.lua
+++ spec/unpack/Inflater-spec.lua
@@ -1,5 +1,29 @@
+local Inflater = require('weapon/interpreted/Inflater')
+
 describe('Inflater', function() -- Should return a list of objects that match the properties and methods passed in. Error checking?
-    it('should read true as true', function()
-        assert.true(true)
+    local propertySettingMethods
+    setup(function()
+        _G.class = require 'lib/middleclass'
+        DummyWeaponObj = require 'spec/weapons/DummyWeaponObj'
+    end)
+
+
+
+    describe('Test 1', function()
+        local propertySettingMethods = {}
+        local instantiator = Inflater.newObjectsOfSingleTypeInstantiator()
+        it('should read true as true', function()
+            assert.true(true)
+        end)
+    end)
+    describe('Test 2', function()
+        -- "Archibald":{
+        --     "Type": "Asparagus",
+        --     "Height": "6'3",
+        --     "Clothes":[
+        --         "Bow Tie",
+        --         "Monacle"
+        --     ]
+        -- }
     end)
 end)
