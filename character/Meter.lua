6413781e1f1494a8ed52918f2ab59c95468705b5
Micah Argeropoulos <spaceman.bob@live.com>
2024-05-11 17:52:03 -0500

    Update Meter.lua
    fix bug in meter color levels

--- character/Meter.lua
+++ character/Meter.lua
@@ -1,8 +1,9 @@
 function shouldTextBeDark(cap,level,backColor,fillColor)
     local backColorRGBValue = backColor[1] + backColor[2] + backColor[3]
     local fillColorRGBValue = fillColor[1] + fillColor[2] + fillColor[3]
-    local totalBack = cap/level * backColorRGBValue
-    local totalFill = level/cap * fillColorRGBValue
+    local percentage = level/cap
+    local totalBack = (1 - percentage) * backColorRGBValue
+    local totalFill = percentage * fillColorRGBValue
     local averageRGBValue = totalBack + totalFill
     return averageRGBValue > 0.8
 end
@@ -18,6 +19,8 @@ return {
         else
             love.graphics.setColor(1,1,1)
         end
-        love.graphics.print(level, x + width/2 - font:getWidth(level)/2, y + height/2 - font:getHeight()/2)
+        local printx = x + width/2 - font:getWidth(level)/2
+        local printy = y + height/2 - font:getHeight()/2
+        love.graphics.print(level, printx, printy)
     end
 }
