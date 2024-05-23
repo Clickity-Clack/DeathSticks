863c9193385d32f93b6ce2b6745930f2800e770c
Micah Argeropoulos <spaceman.bob@live.com>
2024-05-23 18:30:51 -0500

    clean up

--- world/WorldFactory.lua
+++ world/WorldFactory.lua
@@ -1,25 +1,4 @@
-local Platform = require 'platform/Platform'
-local DestroyablePlatform = require 'platform/DestroyablePlatform'
-local TeamBase = require 'platform/TeamBase'
-local DeadlyPlatform = require 'platform/DeadlyPlatform'
-local Water = require 'platform/Water'
-local Bottom = require 'platform/Bottom'
 local CharacterControllable = require 'character/CharacterControllable'
-local NullControllable = require 'character/NullControllable'
-local FingerBullet = require 'weapons/projectiles/FingerBullet'
-local ThirtyOdd = require 'weapons/projectiles/ThirtyOdd'
-local Pointer = require 'weapons/Pointer'
-local Sniper = require 'weapons/Sniper'
-local Shotgun = require 'weapons/Shotgun'
-local RocketLauncher = require 'weapons/RocketLauncher'
-local GrenadeLauncher = require 'weapons/GrenadeLauncher'
-local Character = require 'character/Character'
-local HealthPower = require 'powerups/HealthPower'
-local WeaponPower = require 'powerups/WeaponPower'
-local ArmorPower = require 'powerups/ArmorPower'
-local JetpackPower = require 'powerups/JetpackPower'
-local winWidth = love.graphics.getWidth
-local winHeight = love.graphics.getHeight
 local GridBackDrop = require 'world/backdrops/GridBackDrop'
 local necromancer = require 'handlers/necromancer'
 
@@ -28,7 +7,6 @@ local WorldFactory = class('WorldFactory')
 function WorldFactory:PopulateWorld(world, mapName)
     world.backDrop = GridBackDrop:new()
     world.controllable = CharacterControllable
-    world.stemTypes = {CharacterControllable = true, ThirtyOdd = true, NineMil = true, Twelve = true, Rocket = true, Grenade = true, Explosion = true, HealthPower = true, WeaponPower = true, ArmorPower = true, JetpackPower = true, FingerBullet = true, NullControllable = true, Platform = true, DestroyablePlatform = true, DeadlyPlatform = true, Bottom = true}
     loadMap(mapName, world.stems, world.physicsWorld, world.spawnPoints)
 end
 
